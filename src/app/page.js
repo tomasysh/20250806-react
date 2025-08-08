e8d3f8fb4abda5284cec13583f48870c50162d25
tomasysh <tomasysh@gmail.com>
2025-08-08 14:54:40 +0800

    feat(layout): 新增 Hero 元件至 About 和 Home 頁面佈局

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
+import { Hero } from "@/components/Layout";
+
 export default function Home() {
   return (
     <>
-      {/* Page Header*/}
-
-      {/* Main Content*/}
+      <Hero title="A Blog Theme by Start Bootstrap" img="home-bg" />
       <div className="container px-4 px-lg-5">
         <div className="row gx-4 gx-lg-5 justify-content-center">
           <div className="col-md-10 col-lg-8 col-xl-7">
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -46,20 +46,18 @@ export const Navbar = () => {
   );
 };
 
-export const Hero = () => {
+export const Hero = ({ title, img }) => {
   return (
     <header
       className="masthead"
-      style={{ backgroundImage: 'url("/img/home-bg.jpg")' }}
+      style={{ backgroundImage: 'url("/img/' + img + '.jpg")' }}
     >
       <div className="container position-relative px-4 px-lg-5">
         <div className="row gx-4 gx-lg-5 justify-content-center">
           <div className="col-md-10 col-lg-8 col-xl-7">
             <div className="site-heading">
               <h1>Clean Blog</h1>
-              <span className="subheading">
-                A Blog Theme by Start Bootstrap
-              </span>
+              <span className="subheading">{title}</span>
             </div>
           </div>
         </div>
