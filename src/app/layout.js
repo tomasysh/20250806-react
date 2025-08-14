f66cd28c80b465c3838f1dd36b80ed93dd1fdf90
tomasysh <tomasysh@gmail.com>
2025-08-14 11:03:42 +0800

    feat(layout): 新增 Bootstrap 支援及動態引入功能 (fix 漢堡選單無法正常開闔)

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar, Hero, Footer } from "@/components/Layout";
+import BootstrapClient from "@/providers/BootstrapClient";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,6 +21,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <BootstrapClient />
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Navbar />
         {children}
