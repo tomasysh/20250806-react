6ec9cf86c8cbcd05564c8562de17a6a9ac7f0c6d
tomasysh <tomasysh@gmail.com>
2025-08-15 11:24:04 +0800

    feat(layout): 使用 Next.js 的 Link 元件替換導航連結

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light" id="mainNav">
       <div className="container px-4 px-lg-5">
         <a className="navbar-brand" href="index.html">
-          Start Bootstrap1
+          Start Bootstrap
         </a>
         <button
           className="navbar-toggler"
@@ -15,19 +17,19 @@ export const Navbar = () => {
           aria-label="Toggle navigation"
         >
           Menu
-          <i className="fas fa-bars" />
+          <i className="fas fa-bars"></i>
         </button>
         <div className="collapse navbar-collapse" id="navbarResponsive">
           <ul className="navbar-nav ms-auto py-4 py-lg-0">
             <li className="nav-item">
-              <a className="nav-link px-lg-3 py-3 py-lg-4" href="index.html">
+              <Link className="nav-link px-lg-3 py-3 py-lg-4" href="/">
                 Home
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link px-lg-3 py-3 py-lg-4" href="about.html">
+              <Link className="nav-link px-lg-3 py-3 py-lg-4" href="/about">
                 About
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
               <a className="nav-link px-lg-3 py-3 py-lg-4" href="post.html">
